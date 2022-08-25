a526a42377e4b94781b4ab611a60d93c28cdd778
fouziabenadida <benadiafouzia@gmail.com>
2022-08-25 19:25:17 +0300

    first commit

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,17 +1,43 @@
 <template>
-<div>
-  <template v-if="this.question">
-    <h1 v-html="this.question"></h1>
-   <ul>
-    <li v-for="(answer, index) in this.answers" :key="index">
-      <input type="radio" name="options" :value="answer" v-model="this.chosen_answer" />
+  <div>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+      <ul>
+        <li v-for="(answer, index) in this.answers" :key="index">
+          <input
+            type="radio"
+            name="options"
+            :value="answer"
+            v-model="this.chosenAnswer"
+          />
 
-      <label v-html="answer"></label>
-    </li>
-    </ul>
-     <button @click="submitAnswer" class="send" type="button">Send</button>
-  </template>
- </div>
+          <label v-html="answer"></label>
+        </li>
+      </ul>
+      <button
+        v-if="!this.answerSubmitted"
+        @click="submitAnswer"
+        class="send"
+        type="button"
+      >
+        Send
+      </button>
+
+      <section v-if="this.answerSubmitted" class="result">
+        <h4 vh  v-if="this.chosenAnswer === this.correctAnswers" v-html="  '&#9989; Congratulations, the answer' +  this.correctAnswers + 
+          'is correct. '" >
+        
+        </h4>
+        <h4 v-else v-html=" ' &#10060; I`m sorry, you picked the wrong answer. The right one is:' +
+            this.correctAnswers + '.'
+          ">
+        
+        </h4>
+
+        <button @click="this. getNewQuetion()" class="send" type="button">Next Question</button>
+      </section>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -22,15 +48,38 @@ export default {
       question: undefined,
       correctAnswers: undefined,
       incorrectAnswers: undefined,
-      chosen_answer:undefined
+      chosenAanswer: undefined,
+      answerSubmitted: false,
     };
   },
-  methods:{
-   submitAnswer (){
-    if (!this.chosen_answer) {
-        alert('Pick one of the options')
-    }
-   }
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert("Pick one of the options");
+      } else {
+        this.answerSubmitted = true;
+        if (this.chosenAnswer == this.correctAnswers) {
+          console.log("Bravo!! ");
+        } else {
+          console.log("you got it wrong");
+        }
+      }
+    },
+    getNewQuetion  () {
+        this.question= undefined,
+        this.answerSubmitted= false,
+        this.chosenAanswer= undefined
+
+      this.axios
+        .get("https://opentdb.com/api.php?amount=1&category=18")
+        .then((response) => {
+          (this.question = response.data.results[0].question),
+            (this.correctAnswers = response.data.results[0].correct_answer),
+            (this.incorrectAnswers =
+              response.data.results[0].incorrect_answers);
+          console.log(response.data.results[0]);
+        });
+    },
   },
 
   computed: {
@@ -46,14 +95,7 @@ export default {
   },
 
   created() {
-    this.axios
-      .get("https://opentdb.com/api.php?amount=1&category=18")
-      .then((response) => {
-        (this.question = response.data.results[0].question),
-          (this.correctAnswers = response.data.results[0].correct_answer),
-          (this.incorrectAnswers = response.data.results[0].incorrect_answers);
-        console.log(response.data.results[0]);
-      });
+    this.getNewQuetion();
   },
 };
 </script>
@@ -75,15 +117,12 @@ input [type="radio"] {
   background-color: #1867c0;
   cursor: pointer;
 }
-ul{
-    list-style-type: none;
-   
+ul {
+  list-style-type: none;
 }
-li{
-   
-    margin-top: 10px;
-    margin-bottom: 10px;
-    text-align: center;
-
+li {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: center;
 }
 </style>
